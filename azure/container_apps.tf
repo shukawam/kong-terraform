7ee8f1c2b7f384eb76168da1cc545f382bc71c61
shukawam <shukawam+github@gmail.com>
2025-07-07 15:03:24 +0900

    Remove sample container app

--- azure/container_apps.tf
+++ azure/container_apps.tf
@@ -1,20 +1,20 @@
-resource "azurerm_container_app_environment" "shukawam_acp_env" {
-  name                = "shukawam-acp-env"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.shukawam_resource_group.name
-}
+# resource "azurerm_container_app_environment" "shukawam_acp_env" {
+#   name                = "shukawam-acp-env"
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.shukawam_resource_group.name
+# }
 
-resource "azurerm_container_app" "shukawam_acp" {
-  name                         = "shukawam-acp"
-  container_app_environment_id = azurerm_container_app_environment.shukawam_acp_env.id
-  resource_group_name          = azurerm_resource_group.shukawam_resource_group.name
-  revision_mode                = "Single"
-  template {
-    container {
-      name   = "gin-api"
-      image  = "kennethreitz/httpbin:latest"
-      cpu    = "0.25"
-      memory = "0.5Gi"
-    }
-  }
-}
+# resource "azurerm_container_app" "shukawam_acp" {
+#   name                         = "shukawam-acp"
+#   container_app_environment_id = azurerm_container_app_environment.shukawam_acp_env.id
+#   resource_group_name          = azurerm_resource_group.shukawam_resource_group.name
+#   revision_mode                = "Single"
+#   template {
+#     container {
+#       name   = "gin-api"
+#       image  = "kennethreitz/httpbin:latest"
+#       cpu    = "0.25"
+#       memory = "0.5Gi"
+#     }
+#   }
+# }

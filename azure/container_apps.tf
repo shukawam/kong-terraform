e7fde715e5f0add25a5c2bf5ab6c1dbcce63d701
shukawam <shukawam+github@gmail.com>
2025-07-07 15:56:36 +0900

    Added container app env

--- azure/container_apps.tf
+++ azure/container_apps.tf
@@ -1,8 +1,8 @@
-# resource "azurerm_container_app_environment" "shukawam_acp_env" {
-#   name                = "shukawam-acp-env"
-#   location            = var.location
-#   resource_group_name = azurerm_resource_group.shukawam_resource_group.name
-# }
+resource "azurerm_container_app_environment" "shukawam_acp_env" {
+  name                = "shukawam-acp-env"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.shukawam_resource_group.name
+}
 
 # resource "azurerm_container_app" "shukawam_acp" {
 #   name                         = "shukawam-acp"
@@ -15,6 +15,26 @@
 #       image  = "kennethreitz/httpbin:latest"
 #       cpu    = "0.25"
 #       memory = "0.5Gi"
+#     }
+#   }
+# }
+
+# resource "azurerm_container_app" "shukawam-kong-gw" {
+#   name                         = "shukawam-kong-gw"
+#   container_app_environment_id = azurerm_container_app_environment.shukawam_acp_env.id
+#   resource_group_name          = azurerm_resource_group.shukawam_resource_group.name
+#   revision_mode                = "Single"
+#   template {
+#     container {
+#       name   = "kong-gateway"
+#       image  = "kong/kong-gateway:3.10"
+#       cpu    = "1"
+#       memory = "2Gi"
+#       env {
+#         name = "KONG_ROLE"
+#         value = "data_plane"
+#       }
+#       env {secret_name = ""}
 #     }
 #   }
 # }

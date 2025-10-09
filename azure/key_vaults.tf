0525c0bd475f7e4e8d5e794e0953f3621b68218f
shukawam <shukawam+github@gmail.com>
2025-10-09 13:40:13 +0900

    Added depends on

--- azure/key_vaults.tf
+++ azure/key_vaults.tf
@@ -5,8 +5,8 @@ resource "azurerm_key_vault" "shukawam_key_vault" {
   sku_name            = "standard"
   tenant_id           = var.tenant_id
   access_policy {
-    tenant_id      = data.azurerm_client_config.current.tenant_id
-    object_id      = data.azurerm_client_config.current.object_id
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
     secret_permissions = [
       "Backup",
       "Delete",
@@ -24,33 +24,39 @@ resource "azurerm_key_vault_secret" "kong_cluster_control_plane" {
   key_vault_id = azurerm_key_vault.shukawam_key_vault.id
   name         = "kong-cluster-control-plane"
   value        = var.kong_cluster_control_plane
+  depends_on   = [azurerm_key_vault.shukawam_key_vault]
 }
 
 resource "azurerm_key_vault_secret" "kong_cluster_server_name" {
   key_vault_id = azurerm_key_vault.shukawam_key_vault.id
   name         = "kong-cluster-server-name"
   value        = var.kong_cluster_server_name
+  depends_on   = [azurerm_key_vault.shukawam_key_vault]
 }
 
 resource "azurerm_key_vault_secret" "kong_cluster_telemetry_endpoint" {
   key_vault_id = azurerm_key_vault.shukawam_key_vault.id
   name         = "kong-cluster-telemetry-endpoint"
   value        = var.kong_cluster_telemetry_endpoint
+  depends_on   = [azurerm_key_vault.shukawam_key_vault]
 }
 
 resource "azurerm_key_vault_secret" "kong_cluster_telemetry_server_name" {
   key_vault_id = azurerm_key_vault.shukawam_key_vault.id
   name         = "kong-cluster-telemetry-server-name"
   value        = var.kong_cluster_telemetry_server_name
+  depends_on   = [azurerm_key_vault.shukawam_key_vault]
 }
 resource "azurerm_key_vault_secret" "kong_cluster_cert" {
   key_vault_id = azurerm_key_vault.shukawam_key_vault.id
   name         = "kong-cluster-cert"
   value        = var.kong_cluster_cert
+  depends_on   = [azurerm_key_vault.shukawam_key_vault]
 }
 
 resource "azurerm_key_vault_secret" "kong_cluster_cert_key" {
   key_vault_id = azurerm_key_vault.shukawam_key_vault.id
   name         = "kong-cluster-cert-key"
   value        = var.kong_cluster_cert_key
+  depends_on   = [azurerm_key_vault.shukawam_key_vault]
 }

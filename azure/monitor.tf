7467d470e201cee2dcb27d5e25eaf9e6da78beb2
shukawam <shukawam+github@gmail.com>
2025-10-09 17:41:35 +0900

    Added log analytics workspace

--- azure/monitor.tf
+++ azure/monitor.tf
@@ -4,6 +4,12 @@ resource "azurerm_monitor_workspace" "shukawam_azure_monitor_workspace" {
   resource_group_name = azurerm_resource_group.shukawam_resource_group.name
 }
 
+resource "azurerm_log_analytics_workspace" "shukawam_log_analytics_workspace" {
+  name     = "shukawam-log-analytics-workspace"
+  location = var.location
+  resource_group_name = azurerm_resource_group.shukawam_resource_group.name
+}
+
 resource "azurerm_application_insights" "shukawam_application_insights" {
   name                = "shukawam-application-insights"
   location            = var.location

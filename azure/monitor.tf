472b8d610b07a2963392864e17cff2fb545fe3b1
shukawam <shukawam+github@gmail.com>
2025-10-09 15:39:01 +0900

    Added app insights

--- azure/monitor.tf
+++ azure/monitor.tf
@@ -3,3 +3,11 @@ resource "azurerm_monitor_workspace" "shukawam_azure_monitor_workspace" {
   location            = var.location
   resource_group_name = azurerm_resource_group.shukawam_resource_group.name
 }
+
+resource "azurerm_application_insights" "shukawam_application_insights" {
+  name                = "shukawam-application-insights"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.shukawam_resource_group.name
+  application_type    = "web"
+  workspace_id        = azurerm_monitor_workspace.shukawam_azure_monitor_workspace.id
+}

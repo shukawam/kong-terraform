fa9e7db48011cf920d6711d37eb22fe7aa3458a9
shukawam <shukawam+github@gmail.com>
2025-10-09 10:37:37 +0900

    Update access policy

--- azure/key_vaults.tf
+++ azure/key_vaults.tf
@@ -4,6 +4,19 @@ resource "azurerm_key_vault" "shukawam_key_vault" {
   resource_group_name = azurerm_resource_group.shukawam_resource_group.name
   sku_name            = "standard"
   tenant_id           = var.tenant_id
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "Get",
+    ]
+    secret_permissions = [
+      "Get",
+    ]
+    storage_permissions = [
+      "Get",
+    ]
+  }
 }
 
 resource "azurerm_key_vault_secret" "kong_cluster_cert" {

a0066f753ba6a31dcdafe75012663170853d225d
shukawam <shukawam+github@gmail.com>
2025-10-14 21:10:06 +0900

    Added azure monitor diagnostic setting

--- azure/monitor.tf
+++ azure/monitor.tf
@@ -16,3 +16,19 @@ resource "azurerm_application_insights" "shukawam_application_insights" {
   resource_group_name = azurerm_resource_group.shukawam_resource_group.name
   application_type    = "web"
 }
+
+# Monitor Diagnostic Setting for Container App to Log Analytics
+resource "azurerm_monitor_diagnostic_setting" "shukawam_container_app_diagnostic_setting" {
+  name                       = "shukawam-kong-gateway-logs"
+  target_resource_id         = azurerm_container_app.shukawam-kong-gateway.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.shukawam_log_analytics_workspace.id
+  enabled_log {
+    category = "SystemLogs"
+  }
+  enabled_log {
+    category = "ContainerAppConsoleLogs"
+  }
+  enabled_log {
+    category = "ContainerAppSystemLogs"
+  }
+}

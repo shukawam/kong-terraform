c826aeae64c13acf5ede6af8af2fa3fc1544996f
shukawam <shukawam+github@gmail.com>
2025-10-17 16:13:45 +0900

    Delete key-value

--- azure/key_vault.tf
+++ azure/key_vault.tf
@@ -1,78 +1,78 @@
-resource "azurerm_key_vault" "shukawam_key_vault" {
-  name                        = "shukawam-key-vault"
-  location                    = var.location
-  resource_group_name         = azurerm_resource_group.shukawam_resource_group.name
-  tenant_id                   = var.tenant_id
-  sku_name                    = "standard"
-  enable_rbac_authorization   = false
-  access_policy {
-    tenant_id = var.tenant_id
-    object_id = var.user_id
-    key_permissions = [
-      "Backup",
-      "Create",
-      "Decrypt",
-      "Delete",
-      "Encrypt",
-      "Get",
-      "Import",
-      "List",
-      "Purge",
-      "Recover",
-      "Restore",
-      "Sign",
-      "UnwrapKey",
-      "Update",
-      "Verify",
-      "WrapKey",
-      "Release",
-      "Rotate",
-      "GetRotationPolicy",
-      "SetRotationPolicy"
-    ]
-    secret_permissions = [
-      "Backup",
-      "Delete",
-      "Get",
-      "List",
-      "Purge",
-      "Recover",
-      "Restore",
-      "Set",
-    ]
-    certificate_permissions = [
-      "Backup",
-      "Create",
-      "Delete",
-      "DeleteIssuers",
-      "Get",
-      "GetIssuers",
-      "Import",
-      "List",
-      "ListIssuers",
-      "ManageContacts",
-      "ManageIssuers",
-      "Purge",
-      "Recover",
-      "Restore",
-      "SetIssuers",
-      "Update"
-    ]
-    storage_permissions = [
-      "Backup",
-      "Delete",
-      "DeleteSAS",
-      "Get",
-      "GetSAS",
-      "List",
-      "ListSAS",
-      "Purge",
-      "Recover",
-      "RegenerateKey",
-      "Restore",
-      "Set",
-      "SetSAS",
-      "Update"
-    ]
-  }
-}
+# resource "azurerm_key_vault" "shukawam_key_vault" {
+#   name                        = "shukawam-key-vault"
+#   location                    = var.location
+#   resource_group_name         = azurerm_resource_group.shukawam_resource_group.name
+#   tenant_id                   = var.tenant_id
+#   sku_name                    = "standard"
+#   enable_rbac_authorization   = false
+#   access_policy {
+#     tenant_id = var.tenant_id
+#     object_id = var.user_id
+#     key_permissions = [
+#       "Backup",
+#       "Create",
+#       "Decrypt",
+#       "Delete",
+#       "Encrypt",
+#       "Get",
+#       "Import",
+#       "List",
+#       "Purge",
+#       "Recover",
+#       "Restore",
+#       "Sign",
+#       "UnwrapKey",
+#       "Update",
+#       "Verify",
+#       "WrapKey",
+#       "Release",
+#       "Rotate",
+#       "GetRotationPolicy",
+#       "SetRotationPolicy"
+#     ]
+#     secret_permissions = [
+#       "Backup",
+#       "Delete",
+#       "Get",
+#       "List",
+#       "Purge",
+#       "Recover",
+#       "Restore",
+#       "Set",
+#     ]
+#     certificate_permissions = [
+#       "Backup",
+#       "Create",
+#       "Delete",
+#       "DeleteIssuers",
+#       "Get",
+#       "GetIssuers",
+#       "Import",
+#       "List",
+#       "ListIssuers",
+#       "ManageContacts",
+#       "ManageIssuers",
+#       "Purge",
+#       "Recover",
+#       "Restore",
+#       "SetIssuers",
+#       "Update"
+#     ]
+#     storage_permissions = [
+#       "Backup",
+#       "Delete",
+#       "DeleteSAS",
+#       "Get",
+#       "GetSAS",
+#       "List",
+#       "ListSAS",
+#       "Purge",
+#       "Recover",
+#       "RegenerateKey",
+#       "Restore",
+#       "Set",
+#       "SetSAS",
+#       "Update"
+#     ]
+#   }
+# }

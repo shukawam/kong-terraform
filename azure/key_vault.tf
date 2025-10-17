942ae2b3d7ebfa9cdf08c503919b56d4b305e048
shukawam <shukawam+github@gmail.com>
2025-10-17 16:09:39 +0900

    Fixed key-vault

--- azure/key_vault.tf
+++ azure/key_vault.tf
@@ -4,7 +4,6 @@ resource "azurerm_key_vault" "shukawam_key_vault" {
   resource_group_name         = azurerm_resource_group.shukawam_resource_group.name
   enabled_for_disk_encryption = true
   tenant_id                   = var.tenant_id
-  soft_delete_retention_days  = 7
   purge_protection_enabled    = false
   sku_name                    = "standard"
   enable_rbac_authorization   = false
